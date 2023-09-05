c40dbbdcd889a1f0a69b7a6b2e04c9d2eb08a4cf
Pavel Karpovich <pavel.karpovich@outlook.com>
2023-09-06 00:21:43 +0200

    refactor(image_prompts_generator_prompt.py): move character description to a more appropriate location
    The main character description was moved to a more appropriate location in the prompt to improve readability and comprehension. This change was made to ensure that the character's description is read before the instructions on how to generate the image prompt.

--- little_turtle/prompts/image_prompts_generator_prompt.py
+++ little_turtle/prompts/image_prompts_generator_prompt.py
@@ -1,12 +1,12 @@
 IMAGE_PROMPTS_GENERATOR_PROMPT = """\
 Based on the story, you should generate an image prompt for the image generation tool.
  
-The target image should look like a children's book illustration in Pixar style. 
+The target image should look like a children's book illustration in Pixar style.
+The main character in every story is a charming little turtle who always wears a hat. He may be somewhat clumsy, but he's incredibly kind, pritty and friendly. Turtle should looks young and cute.
 Your prompt must be extremely detailed, specific, and imaginative, in order to generate the most unique and creative images possible.
 This prompt must be imaginative and descriptive, extrapolating information from the story input provided, such as subjects, image medium, composition, environment, lighting, colors, mood and tone, and likeness
 Avoid unuseful formulations, since the noun keywords are the most important, so try to limit yourself to the most important concepts.
 Include details like angle, materials, and technique to get a more accurate and desirable result
-In every story, there should be a little turtle in the hat.
 
 Story example 1:
 {story_example_1}
