ca44c8400ac72e952bdc0565c39d1c641056d2ed
Pavel Karpovich <pavel.karpovich@outlook.com>
2023-11-14 00:28:02 +0100

    refactor(image_prompts_generator_prompt.py): simplify and clarify prompt generation instructions
    The instructions for generating image prompts were overly complex and contained unnecessary information. This commit simplifies the instructions, making them easier to understand and follow. It also emphasizes the importance of the main character (a turtle) and the style of the prompt (children's book illustration in a Pixar-like style).

--- little_turtle/prompts/image_prompts_generator_prompt.py
+++ little_turtle/prompts/image_prompts_generator_prompt.py
@@ -1,33 +1,18 @@
 IMAGE_PROMPTS_GENERATOR_PROMPT = """\
-Based on the story, you should generate an image prompt for the image generation tool.
+Image that you are prompt engineer, who write prompts for DALL-E generative model, based on the input story and following rules:
 
-The iconic prompt is:
-Illustration, children's picture book, cute little turtle in the hat (green color), <turtle story action> <background scene>. Pixar style, whole body, cute, smiling, detailed, ultra high definition, 8k, --q 2 {model_version}
+Step 1:
+Read the input story and extract the historical event from it. Ignore all additional information. The story may contain only one historical event
 
-Before you start replacing parts, you should envision the scenario and then describe it in great detail using substitution rules.
+Step 2:
+Describe the historical event in as much detail as possible to visualize the story based on the following rules:
 
-Replacement rules:
-1. In the narrative, if we encounter comparisons like "moves like a train", we should avoid incorporating them into the resulting prompts. This is because the generator struggles to process such input effectively and might mistakenly transform our turtle into a train. Instead, it would be more appropriate if the turtle played with a toy train, utilizes the train, or operates within the train.
-2. Replacement parts should be creative and detailed, drawing information from the story input, such as subjects, composition, environment, colors, mood, and tone.
-3. <turtle story action> and <background scene> parts should always be replaced with a story action and a scene description, respectively.
-4. You don't have any length restrictions. You can use as many words as you want to describe the scene and the action.
-5. Replacement parts should be in the same order as in the original prompt. Strictly forbidden to add new parts to the prompt.
-6. Avoid unnecessary formulations, as the noun keywords are the most important. Try to limit yourself to the most important concepts
-
-Forbidden replacements actions:
-1. Transforming the main character into any other subject, object, or character is not allowed
-2. Using current date or time information is not allowed. Only the use of dates to denote the time of day, century, or year is permitted from the story
-3. Diverting attention from the main character to any other character is not permitted
-
-From original prompt you should replace:
-1. <turtle story action> with a story action based in the input story.
-2. <background scene> with a story scene description.
-
-Avoiding replacement rules and performing forbidden replacement actions are strictly prohibited
-The result should be a prompt with the replacement parts filled in. Do not add any comments or other text to the result
+## Prompt generation rules:
+### 1. Prompt should be in style of children's book illustration and in Pixar-like style
+### 2. Main character of the story is a turtle. The turtle described in the story should be a young and adorable green turtle, with bright, wide eyes and a small, friendly smile. Turtle always wears whimsical hat
 
 Input story:
 {new_story}
 
-Resulting prompt:
+Resulting prompt, refrain from displaying any intermediate information:
 """
